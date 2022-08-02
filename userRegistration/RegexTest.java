7fff587549ab4287037326f572a778cc488c0f6e
rutuja_mali <rutujamali111@gmail.com>
2022-08-02 22:05:30 +0530

    All email validation test cases

--- userRegistration/RegexTest.java
+++ userRegistration/RegexTest.java
@@ -1,9 +1,16 @@
 package userRegistration;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class RegexTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int i=0;
+		HashMap<String, Boolean> map=new HashMap<String, Boolean>();
 		Regex reg=new Regex();
 		String finame = "Rutuja"; 
         System.out.println(reg.isValidFirstName(finame));
@@ -11,8 +18,23 @@ public class RegexTest {
         String liname = "mali"; 
         System.out.println(reg.isValidLastName(liname)); 
         
-        String mail = "abc.xyz@bl.co.in"; 
-        System.out.println(reg.isValidMail(mail)); 
+        System.out.println("Enter mail ID: ");
+        while(i<21)
+        {
+        	String mail = sc.next(); 
+            //System.out.println(reg.isValidMail(mail)); 
+            map.put(mail, reg.isValidMail(mail));
+            i++;
+        }
+        System.out.println("mail ID validations test cases:");
+       
+        for(Map.Entry<String, Boolean> element:map.entrySet())
+        {
+        	String key=element.getKey();
+        	boolean result=element.getValue();
+        	System.out.println(key+"    "+result);
+        
+        }
         
         String no = "91 9309813565"; 
         System.out.println(reg.isValidNo(no)); 
